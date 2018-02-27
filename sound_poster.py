5645bec37adbb7c31dbacfe23609f91f5c68a16f
chris <celers.dev@gmail.com>
2018-02-27 14:55:44 +0100

    sound type output

--- sound_poster.py
+++ sound_poster.py
@@ -2,9 +2,17 @@ import requests
 import sys
 from playsound import playsound
 
+sound_names = ["air conditioner", "car horn", "children playing", "dog bark", "drilling", "engine idling","gun shot", "jackhammer", "siren", "street music"]
+
+
 playsound(sys.argv[1])
 url = 'http://127.0.0.1:5000/upload'
 files = {'file': open(sys.argv[1], 'rb')}
 r = requests.post(url, files=files)
 print(r.text)
+index = int(r.text)
+if index < 0 or index >= len(sound_names):
+    print("Error, index out of range")
+else:
+    print(sound_names[index])
 
